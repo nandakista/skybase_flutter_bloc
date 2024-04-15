358031a8ba30d64e7bc41a07843aa6f95c94ba2a
nanda.kista <nanda.kista@gmail.com>
2024-04-15 13:53:03 +0700

    Integrate hydrated bloc in SampleFeature Detail

--- lib/data/repositories/sample_feature/sample_feature_repository_impl.dart
+++ lib/data/repositories/sample_feature/sample_feature_repository_impl.dart
@@ -4,7 +4,6 @@ import 'package:dio/dio.dart';
 import 'package:skybase/config/base/base_repository.dart';
 import 'package:skybase/data/models/sample_feature/sample_feature.dart';
 import 'package:skybase/data/repositories/sample_feature/sample_feature_repository.dart';
-import 'package:skybase/data/sources/local/cached_key.dart';
 import 'package:skybase/data/sources/server/sample_feature/sample_feature_sources.dart';
 
 class SampleFeatureRepositoryImpl extends BaseRepository
@@ -23,22 +22,22 @@ class SampleFeatureRepositoryImpl extends BaseRepository
   }) async {
     try {
       // Using cached
-      return await loadCachedList(
-        cachedKey: CachedKey.SAMPLE_FEATURE_LIST,
-        page: page,
-        onLoad: () async => await apiService.getUsers(
-          cancelToken: cancelToken,
-          page: page,
-          perPage: perPage,
-        ),
-      );
+      // return await loadCachedList(
+      //   cachedKey: CachedKey.SAMPLE_FEATURE_LIST,
+      //   page: page,
+      //   onLoad: () async => await apiService.getUsers(
+      //     cancelToken: cancelToken,
+      //     page: page,
+      //     perPage: perPage,
+      //   ),
+      // );
 
       // Without cache
-      // return await apiService.getUsers(
-      //   cancelToken: requestParams.cancelToken,
-      //   page: page,
-      //   perPage: perPage,
-      // );
+      return await apiService.getUsers(
+        cancelToken: cancelToken,
+        page: page,
+        perPage: perPage,
+      );
     } catch (e, stack) {
       log('$tag error = $e, $stack');
       rethrow;
@@ -52,49 +51,23 @@ class SampleFeatureRepositoryImpl extends BaseRepository
     required String username,
   }) async {
     try {
-      // Using cache
-      return await loadCached(
-        cachedKey: CachedKey.SAMPLE_FEATURE_DETAIL,
-        cachedId: id.toString(),
-        onLoad: () async => await apiService
-            .getDetailUser(cancelToken: cancelToken, username: username)
-            .then(
-          (res) async {
-            res.followersList = await apiService.getFollowers(
-              cancelToken: cancelToken,
-              username: username,
-            );
-            res.followingList = await apiService.getFollowings(
-              cancelToken: cancelToken,
-              username: username,
-            );
-            res.repositoryList = await apiService.getRepos(
-              cancelToken: cancelToken,
-              username: username,
-            );
-            return res;
-          },
-        ),
+      final SampleFeature res = await apiService.getDetailUser(
+        cancelToken: cancelToken,
+        username: username,
       );
-
-      // Without Cache
-      // final SampleFeature res = await apiService.getDetailUser(
-      //   cancelToken: requestParams.cancelToken,
-      //   username: username,
-      // );
-      // res.followersList = await apiService.getFollowers(
-      //   cancelToken: requestParams.cancelToken,
-      //   username: username,
-      // );
-      // res.followingList = await apiService.getFollowings(
-      //   cancelToken: requestParams.cancelToken,
-      //   username: username,
-      // );
-      // res.repositoryList = await apiService.getRepos(
-      //   cancelToken: requestParams.cancelToken,
-      //   username: username,
-      // );
-      // return res;
+      res.followersList = await apiService.getFollowers(
+        cancelToken: cancelToken,
+        username: username,
+      );
+      res.followingList = await apiService.getFollowings(
+        cancelToken: cancelToken,
+        username: username,
+      );
+      res.repositoryList = await apiService.getRepos(
+        cancelToken: cancelToken,
+        username: username,
+      );
+      return res;
     } catch (e, stack) {
       log('$tag Failed get data $e, $stack');
       rethrow;
--- lib/ui/views/sample_feature/detail/sample_feature_detail_route.dart
+++ lib/ui/views/sample_feature/detail/sample_feature_detail_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:skybase/data/repositories/sample_feature/sample_feature_repository.dart';
 import 'package:skybase/service_locator.dart';
 import 'package:skybase/ui/views/sample_feature/detail/bloc/sample_feature_detail_bloc.dart';
 import 'package:skybase/ui/views/sample_feature/detail/sample_feature_detail_view.dart';
@@ -9,9 +10,12 @@ final sampleFeatureDetailPage = [
     path: SampleFeatureDetailView.route,
     name: SampleFeatureDetailView.route,
     builder: (context, state) {
-      final extra = state.extra as Map<String, dynamic>;
+      final Map<String, dynamic> extra = state.extra as Map<String, dynamic>;
       return BlocProvider(
-        create: (_) => sl<SampleFeatureDetailBloc>(),
+        create: (_) => SampleFeatureDetailBloc(
+          repository: sl<SampleFeatureRepository>(),
+          userId: extra['id'] as int,
+        ),
         child: SampleFeatureDetailView(
           idArgs: extra['id'] as int,
           usernameArgs: extra['username'] as String,
