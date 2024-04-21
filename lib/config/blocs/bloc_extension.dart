ebac125981929263ceb44a79d5e76d52caa34dbe
nanda.kista <nanda.kista@gmail.com>
2024-04-22 06:30:10 +0700

    Add code doc in BaseBloc, PaginationBloc

--- lib/config/blocs/bloc_extension.dart
+++ lib/config/blocs/bloc_extension.dart
@@ -2,8 +2,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 extension BlocExtension<Event, State> on Bloc<Event, State> {
   /// Add bloc event and wait until predicate returns true
-  Future<void> addAndAwait(Event event, bool Function(State state) state) async {
+  Future<void> addAndAwait(
+    Event event,
+    bool Function(State state) predicate,
+  ) async {
     add(event);
-    await stream.firstWhere(state);
+    await stream.firstWhere(predicate);
   }
 }
--- lib/data/sources/local/cached_key.dart
+++
@@ -1,6 +0,0 @@
-class CachedKey {
-  static const SAMPLE_FEATURE_LIST = 'sample_feature_list';
-  static const SAMPLE_FEATURE_DETAIL = 'sample_feature_detail';
-  static const PROFILE = 'user_profile';
-  static const USER_REPOSITORY = 'user_repository';
-}
