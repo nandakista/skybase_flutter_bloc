ff51b0b01eacf3eb10ca8a72d5db659930d19a50
nanda.kista <nanda.kista@gmail.com>
2024-08-27 11:04:44 +0800

    Add Env Banner

--- lib/config/environment/app_env.dart
+++ lib/config/environment/app_env.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:skybase/config/environment/app_configuration.dart';
 
 import 'config_data.dart';
@@ -16,6 +17,18 @@ extension EnvExtension on Environment {
   bool get isStaging => this == Environment.STAGING;
   bool get isDev => this == Environment.DEVELOPMENT;
   bool get isProduction => this == Environment.PRODUCTION;
+
+  String get title => switch(this) {
+    Environment.PRODUCTION => 'PROD',
+    Environment.STAGING => 'STAGING',
+    Environment.DEVELOPMENT => 'DEV',
+  };
+
+  Color get color => switch(this) {
+    Environment.PRODUCTION => Colors.green,
+    Environment.STAGING => Colors.purple,
+    Environment.DEVELOPMENT => Colors.blue,
+  };
 }
 
 class AppEnv {
